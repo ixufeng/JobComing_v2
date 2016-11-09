c7e99f5e3948fcd022d46a079f2f29ff5387b953
xufeng <1054993687>
2016-11-09 09:43:45 +0800

    modify

--- JobComing_v2/src/test/java/com/juntil/test/Test.java
+++ JobComing_v2/src/test/java/com/juntil/test/Test.java
@@ -1,28 +1,2 @@
 package com.juntil.test;
 
-import java.util.Arrays;
-import java.util.Date;
-
-import javax.annotation.Resource;
-
-import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import com.job.bean.Agreement;
-import com.job.bean.Chat;
-import com.job.bean.Evaluate;
-import com.job.dao.AgreementDao;
-import com.job.dao.ChatDao;
-import com.job.dao.EvaluateDao;
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration({"classpath:configs/spring-mybatis.xml"})
-public class Test {
-	@Resource
-	private EvaluateDao evaluateDao;
-	@org.junit.Test
-	public void test(){
-		System.out.println(evaluateDao.getEvaluateByJobUserId(3).size());
-	
-	}
-}
